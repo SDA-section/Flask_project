d0bfa5ade166e63aaed690a788f080183f337494
menna260 <s81289290@gmail.com>
2025-02-19 13:18:08 +0200

    Adding HTML, CSS Files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1 style='color:red'>Hello</h1>"
+    return render_template('index.html', title="Home", style='home.css')
 
 @app.route('/about')
 def about():
-    return "About"
+   return render_template('about.html', title="About US")
 
 if __name__ == "__main__":
     app.run(debug=True,port=3000)
